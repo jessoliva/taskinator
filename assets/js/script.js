7ece331b66e4e7bdbe60e72cc215586ea1f889e7
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-01-15 15:29:16 -0600

    added form and modified event object behavior

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
-}; //must be placed before addEventListener because it needs to be declared first
+var createTaskHandler = function(event) {
+    //event here refers to the submit event
 
-buttonEl.addEventListener("click", createTaskHandler);
+    event.preventDefault();
+  
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}; //must be placed before addEventListener because it needs to be declared first since it's a function expression w a variable
+
+formEl.addEventListener("submit", createTaskHandler);
+//when you click the button or press enter, it runs createTaskHandler
